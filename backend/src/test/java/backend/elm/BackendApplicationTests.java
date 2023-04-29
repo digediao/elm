262560bdabaf0ff9a94e80ba7105231b3fa41a63
低格调 <79647826+digediao@users.noreply.github.com>
2023-04-29 19:57:38 +0800

    Add files via upload

---
+++ backend/src/test/java/backend/elm/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.elm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
